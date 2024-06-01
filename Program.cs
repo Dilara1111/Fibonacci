19d5f383fcf9bb79be047a1b1849f7874a45e7a9
HuseynovaDilara <huseynova.8885@gmail.com>
2024-06-01 15:47:55 +0400

    Task Added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci");
+            int num1 = int.Parse(Console.ReadLine());
+            int a1 = 0;
+            int a2 = 1;
+            for (int i = 0; i < num1; i++)
+            {
+                a1 = a1 + a2;
+                a2 = a1 - a2;
+                Console.WriteLine(a2);
+            }
+        }
+    }
+}
